926147e8dc2d7ef892ec7371d1678b8c8f459b30
Jeff Bai <j.bai@wustl.edu>
2021-01-10 05:37:22 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,16 +1,1 @@
 require 'pry'
-
-def my_all?(collection)
-  i = 0
-  new_collection = []
-  while i < collection.length
-    new_collection << yield(collection[i])
-    i += 1
-  end
-  
-  if new_collection.include?(false)
-    false
-  else
-    true
-  end
-end
